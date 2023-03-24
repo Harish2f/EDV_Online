59565a541cf4a814d07c67e065bd802ec9f3a969
Harish kumar <harish2f@gmail.com>
2023-03-24 13:23:00 +0530

    Improved UI/legends can be added individually

---
+++ src/stories/Home.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Home from '../components/Home/Home';
+
+export default {
+  title: 'Home',
+  component: Home,
+};
+
+export function HomeDemo() {
+  return <Home />;
+}
